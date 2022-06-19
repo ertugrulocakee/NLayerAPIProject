c42b8c8e2de49b7914e3b69482ede2a87e509e53
ertugrulocakee <ertugrulocakee@gmail.com>
2022-06-19 22:53:53 +0300

    Ürün Detayları Denetleyicisi için yeni aksiyonlar tanımlandı.

--- NLayerAPI.API/Controllers/ProductFeatureController.cs
+++ NLayerAPI.API/Controllers/ProductFeatureController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NLayerAPI.API.Filters;
+using NLayerAPI.Core.Concrete;
+using NLayerAPI.Core.DTOs;
 using NLayerAPI.Core.Services;
 
 namespace NLayerAPI.API.Controllers
@@ -36,6 +39,68 @@ namespace NLayerAPI.API.Controllers
             return CreateActionResult(await _productFeatureService.GetProductFeaturesWithProductAsync());
 
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+
+            var productFeatures = await _productFeatureService.GetAllAsync();
+
+            var productFeatureDTOs = _mapper.Map<List<ProductFeatureDTO>>(productFeatures.ToList());
+
+            return CreateActionResult(CustomResponseDTO<List<ProductFeatureDTO>>.Success(200, productFeatureDTOs));
+
+        }
+
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+
+            var productFeature = await _productFeatureService.GetByIdAsync(id);
+
+            var productFeatureDto = _mapper.Map<ProductFeatureDTO>(productFeature);
+
+            return CreateActionResult(CustomResponseDTO<ProductFeatureDTO>.Success(200, productFeatureDto));
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductFeatureDTO productFeatureDTO)
+        {
+
+            var productFeature = await _productFeatureService.AddAsync(_mapper.Map<ProductFeature>(productFeatureDTO));
+
+            var productFeatureDto = _mapper.Map<ProductFeatureDTO>(productFeature);
+
+            return CreateActionResult(CustomResponseDTO<ProductFeatureDTO>.Success(201, productFeatureDto));
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductFeatureDTO productFeatureDTO)
+        {
+
+            await _productFeatureService.UpdateAsync(_mapper.Map<ProductFeature>(productFeatureDTO));
+
+            return CreateActionResult(CustomResponseDTO<NoContentDTO>.Success(204));
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+
+            var productFeature = await _productFeatureService.GetByIdAsync(id);
+
+            await _productFeatureService.RemoveAsync(productFeature);
+
+            return CreateActionResult(CustomResponseDTO<NoContentDTO>.Success(204));
+
+        }
+
 
 
     }
---
+++ NLayerAPI.Service/Validations/ProductFeatureDTOValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using NLayerAPI.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerAPI.Service.Validations
+{
+    public class ProductFeatureDTOValidator : AbstractValidator<ProductFeatureDTO>
+    {
+
+        public ProductFeatureDTOValidator()
+        {
+
+            RuleFor(x => x.Color).NotNull().WithErrorCode("{PropertyName} is required!");
+            RuleFor(x => x.Color).NotEmpty().WithErrorCode("{PropertyName} is required!");
+
+            RuleFor(x => x.Width).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0!");
+            RuleFor(x => x.Height).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0!");
+            RuleFor(x => x.ProductId).InclusiveBetween(1, int.MaxValue).WithMessage("{PropertyName} must be greater 0!");
+
+        }
+
+    }
+}
